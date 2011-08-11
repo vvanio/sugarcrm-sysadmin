07f97acb7b1ccee36b2cab4f42a3dd51dc12b049
B. van Berkum <berend@brixcrm.nl>
2011-08-11 12:31:26 +0200

    Made x11vnc run script read password from ENV or prompt.

--- x11vnc-run.sh
+++ x11vnc-run.sh
@@ -1,1 +1,14 @@
-x11vnc -repeat -gui tray -avahi -passwd testje -noxinerama -nomodtweak -display :0 -http
+#!/bin/bash
+PASSWD=$X11VNC_PASSWD
+if test -z "$PASSWD"
+then
+	echo "Set X11VNC_PASSWD env var for password. "
+	read -t 20 -p "Enter password now: " PASSWD
+fi
+if test -z "$PASSWD"
+then
+	PASSWD=testje
+fi
+
+x11vnc -repeat -gui tray -avahi -passwd $PASSWD -noxinerama -nomodtweak -display :0 -http
+
