aac0b83b0bdbb5446b9b9ba0eb85ad71b074208b
B. van Berkum <berend@brixcrm.nl>
2011-09-22 15:03:59 +0200

    Forgot to add changes to init-symlinks

--- init-symlinks-relative.sh
+++ init-symlinks-relative.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+#
+# Initialize symlinks from .symlinks
+#
+
 do_symlink() # $v
 {
 	# If symlink doesn't exist
 	if test ! -L "$1"
 	then
+		if test -e "$1"
+		then
+			rm -rf $1;
+		fi
 		if test ! -e "$2/$1"
 		then
 			echo "File/folder: $2/$1 doesn't exist"
@@ -25,6 +33,12 @@ do_symlink() # $v
 
 ### Main
 
+if test ! -e sugar_version.php
+then
+	echo "This script must be run from a sugar installation directory. ";
+	exit 1
+fi
+
 # Read file from first arg
 if test -n "$1"; then
 	F=$1
@@ -38,9 +52,17 @@ fi
 
 # Read all lines
 # Whitespace determines 'do_symlink' arguments, paths cannot contain any spaces!
+i=0
 while read line; do
+	i=$(expr $i + 1);
 	# ignore blanks and comments
 	if test -n "$line" -a "${line:0:1}" != "#"; then
-		do_symlink $line `dirname $1`
+		DIR=$(dirname $F)
+		if test -z "$DIR"; then DIR=.; fi
+		if test ! -e "$DIR/$line"; then
+			echo "! Non-existant path $line, given at $F:$i"
+		else
+			do_symlink $line `dirname $1`
+		fi
 	fi
 done < $F
