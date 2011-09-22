b41726a7d23b90b3f44467be37ec787bce61a361
B. van Berkum <berend@brixcrm.nl>
2011-09-22 15:01:18 +0200

    Update symlinks scripts.
    init-symlinks made an error when a path already existed but was not a symlink:
    it added the symlink to the existing directory. Now it will deleted the existing
    path, so be carefull (use version control).
    
    Also, check-symlinks is a new script to check:
    
    - wether paths in a submodule do not exist in the current module yet, and
    - wether lines in the .symlinks file are actually existing paths.
    
    Both should prevent cruft building up and missing paths.

---
+++ check-symlinks.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#
+# Check if .symlinks is complete and not missing paths
+#
+
+BUILD_DIR=.
+if test -z "$1"
+then
+	EXTERNAL=../custom_include
+else
+	EXTERNAL=$1
+fi
+
+if test ! -e $EXTERNAL/.symlinks
+then
+	echo "First argument should point be submodule directory. "
+	exit 1
+fi
+
+if test ! -e sugar_version.php
+then
+	echo "This script must be run from a sugar installation directory. ";
+	exit 1
+fi
+
+echo "Testing if $(dirname $BUILD_DIR) is correctly symlinked to $EXTERNAL"
+
+L=${#EXTERNAL}
+find $EXTERNAL -type f ! -ipath '*.git*' | \
+	while read f; do \
+		O=$(expr $L - ${#f});\
+		F=${f:$O};F=${F#/};\
+		[ ! -e "$F" ] && echo "! Missing link to path: $f" && continue; \
+	done
+
+echo
+echo "Checking wether all lines from $EXTERNAL/.symlinks exists. "
+
+i=0
+while read line; do
+	i=$(expr $i + 1);
+	# ignore blanks and comments
+	if test -n "$line" -a "${line:0:1}" != "#"; then
+		if test -z "$EXTERNAL"; then EXTERNAL=.; fi
+		if test ! -e "$EXTERNAL/$line"; then
+			echo "! Non-existant path $line, given at $EXTERNAL/.symlinks:$i"
+		fi
+	fi
+done < $EXTERNAL/.symlinks
+
