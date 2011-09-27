2982a97ea420e7c19058407b010f335872dde37e
B. van Berkum <berend@dotmpe.com>
2011-09-27 08:34:42 +0200

    Made symlinker slightly more robust, now correctly links dot-files.

--- init-symlinks-relative.sh
+++ init-symlinks-relative.sh
@@ -6,30 +6,26 @@ VERBOSE=0
 # Initialize symlinks from .symlinks
 #
 
-do_symlink() # $sourcepath $targetdir
+do_symlink() # $path $srcdir
 {
-	# If symlink doesn't exist
-	if test ! -L "$1"
+	# If symlink exists continue
+	[ -L "$1" ] && continue;
+	[ -e "$1" ] && (rm $1 || (echo "Path exists: $1" && exit 4));
+	if test ! -e "$2/$1"
 	then
-		if test -d "$1"
+		echo "Symlink path $2/$1 doesn't exist"
+		exit 5
+	else
+		dir=$(dirname $1)
+		[ "$dir" = "." ] && dir = "./";
+		if test ! -d $dir
 		then
-			rm $1 || echo "Path exists: $1" && exit 1;
+			mkdir -p "$dir"
+			echo "Folder: $dir created"
 		fi
-		if test ! -e "$2/$1"
-		then
-			echo "File/folder: $2/$1 doesn't exist"
-			exit 1
-		else
-			dir=`dirname $1`
-			if test ! -d $dir
-			then
-				mkdir -p "$dir"
-				echo "Folder: $dir created"
-			fi
-			# Create the symlink with a relative path
-			ln -s "$(echo $dir | sed -e s/[a-zA-Z\_-]*/../g)/$2/$1" "$1"
-			echo "$1 -> $2/$1"
-		fi
+		# Create the symlink with a relative path
+		ln -s "$(echo $dir | sed -e s/[a-zA-Z\_-]*/../g)/$2/$1" "$1"
+		echo "$1 -> $2/$1"
 	fi
 }
 
@@ -37,8 +33,8 @@ do_symlink() # $sourcepath $targetdir
 
 if test ! -e sugar_version.php
 then
-	echo "This script must be run from a sugar installation directory. ";
-	exit 1
+	echo "This script must be run from a sugar installation root folder. ";
+	exit 3
 fi
 
 # Read file from first arg
@@ -50,12 +46,12 @@ if test -n "$1"; then
 			F=$F/.symlinks
 		else
 			echo "Missing symlinks file: $F";
-			exit 1;
+			exit 2;
 		fi
 	fi
 else
 	echo "Usage: $0 [file]"
-	exit -1
+	exit 1
 fi
 
 # Read all lines
@@ -64,15 +60,17 @@ SRCDIR=$(dirname $F)
 if test -z "$SRCDIR"; then SRCDIR=.; fi
 i=0
 while read line; do
+	# count lines (debugging)
 	i=$(expr $i + 1);
 	# ignore blanks and comments
-	if test -n "$line" -a "${line:0:1}" != "#"; then
-		[ "$VERBOSE" -gt 0 ] && echo 1=$1, SRCDIR=$SRCDIR, line=$line
-		if test ! -e "$SRCDIR/$line"; then
-			echo "! Non-existant path '$line', given at $F:$i"
-		else
-			do_symlink $line $SRCDIR
-		fi
+	[ -z "$(echo $line|sed 's/\s\+//')" -o "${line:0:1}" = "#" ] && continue;
+	# debug print
+	[ "$VERBOSE" -gt 0 ] && echo 1=$1, SRCDIR=$SRCDIR, line=$line
+	# invoke symlink routine
+	if test ! -e "$SRCDIR/$line"; then
+		echo "! Non-existant path '$line', given at $F:$i"
+	else
+		do_symlink $line $SRCDIR
 	fi
 done < $F
 
