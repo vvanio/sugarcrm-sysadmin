762209a06667bf8c0c6040e7bcebe1b792590a95
B. van Berkum <berend@dotmpe.com>
2011-09-27 10:39:35 +0200

    Fixed bug in wildcard reading.

--- init-symlinks-relative.sh
+++ init-symlinks-relative.sh
@@ -54,24 +54,51 @@ else
 	exit 1
 fi
 
-# Read all lines
-# Whitespace determines 'do_symlink' arguments, paths cannot contain any spaces!
 SRCDIR=$(dirname $F)
 if test -z "$SRCDIR"; then SRCDIR=.; fi
+
+# Read all lines, two-pass
+# Whitespace determines 'do_symlink' arguments, paths cannot contain any spaces!
+# Lines with wildcard '*' are expanded first (by echo)
+
+# Expand wildcards, write to temporary file
 i=0
-while read line; do
+while read line
+do
 	# count lines (debugging)
 	i=$(expr $i + 1);
 	# ignore blanks and comments
 	[ -z "$(echo $line|sed 's/\s\+//')" -o "${line:0:1}" = "#" ] && continue;
 	# debug print
 	[ "$VERBOSE" -gt 0 ] && echo 1=$1, SRCDIR=$SRCDIR, line=$line
-	# invoke symlink routine
-	if test ! -e "$SRCDIR/$line"; then
-		echo "! Non-existant path '$line', given at $F:$i"
+	# pick up wildcards in lines
+	if test -n "$(echo ---$line | grep '\*')"
+	then
+		for f in $SRCDIR/$line
+		do
+			l=$(expr ${#SRCDIR} + 1)
+			echo ${f:$l} >> $F.expanded
+		done
 	else
-		do_symlink $line $SRCDIR
+		# test 
+		if test ! -e "$SRCDIR/$line"
+		then
+			echo "! Non-existant path '$line', given at $F:$i"
+			continue
+		else
+			echo $line >> $F.expanded
+		fi
 	fi
-done < $F
+done < $F 
+
+# Now 
+while read line
+do
+	do_symlink $line $SRCDIR
+done < $F.expanded
+
+rm $F.expanded
 
 echo OK.
+exit 0
+
